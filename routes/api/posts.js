c7e720ef9fd3cceeec0ae02add9a03f54461ea6f
Peter Gorgone <peter@peltronic.com>
2019-07-04 12:32:50 -0700

    Completed Ch 28: Add & Remove Comment Routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -11,7 +11,7 @@ const User    = require('../../models/User');
 
 
 // @route   POST api/posts
-// @desc    Create a  post
+// @desc    Create (Add) a post
 // @access   Private
 router.post('/', [ 
     auth, [ 
@@ -158,6 +158,77 @@ router.put('/unlike/:id', auth, async (req, res) => {
         }
 
         res.json(post.likes);
+
+    } catch(err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+// @route    POST api/posts/:id/comments
+// @desc     Comment on a post
+// @access   Private
+router.post('/:id/comment', [ 
+    auth, [ 
+        check('text', 'Text is required').not().isEmpty()
+    ] 
+], 
+async (req, res) => {
+
+    const errors = validationResult(req);
+    if ( !errors.isEmpty() ) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        const post = await Post.findById(req.params.id); // post id from URL
+        const newComment = {
+            text: req.body.text,
+            name: user.name,
+            avatar: user.avatar,
+            user: req.user.id
+        };
+        post.comments.unshift(newComment);
+        await post.save();
+        res.json(post.comments);
+    } catch(err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+// @route    DELETE api/posts/:id/comments/:comment_id
+// @desc     Delete comment on a post
+// @access   Private
+router.delete('/:id/comment/:comment_id', auth, async (req, res) => {
+
+    try {
+        const post = await Post.findById(req.params.id); // post id from URL
+
+        // Pull out comment %TODO %STUDYME
+        const comment = post.comments.find( comment => comment.id===req.params.comment_id ); // returns the comment, or false if not found
+        if (!comment) {
+            return res.status(404).json({ msg: 'Comment not found' });
+        }
+
+        // Check ownership
+        if ( comment.user.toString() !== req.user.id ) {
+            return res.status(401).json({ msg: 'Not authorized' });
+        }
+
+        // Get index to remove...
+        const removeIndex = post.comments.map( comment => {
+            return comment.id.toString();
+        }).indexOf(req.params.comment_id);
+        if (removeIndex >= 0) {
+            post.comments.splice(removeIndex, 1);
+            await post.save();
+        } else {
+            console.error('Illegal index '+removeIndex);
+        }
+
+        res.json(post.comments);
 
     } catch(err) {
         console.error(err.message);
