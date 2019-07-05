26ef62bee4e5d2e558cb18bb99071adcab5f2c7e
Peter Gorgone <peter@peltronic.com>
2019-07-04 18:46:13 -0700

    Completed Ch 38: Alert Reducer, Action & Types

---
+++ client/src/reducers/alert.js
@@ -0,0 +1,21 @@
+import { SET_ALERT, REMOVE_ALERT } from '../actions/types';
+
+const initialState = []; // array of objects
+
+
+export default function(state=initialState, action) {
+
+    const { type, payload } = action;
+
+    // action contains (1) type (required), (2) payload (data)
+    switch (type) {
+        case SET_ALERT:
+            // state is immutable, so you have to include all state
+            return [...state, payload];
+        case REMOVE_ALERT:
+            // remove an alert by ID
+            return state.filter(alert => alert.id !== payload);
+        default:
+            return state;
+    }
+}
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,4 +1,7 @@
+// Root Reducer
 import { combineReducers } from 'redux';
+import alert from './alert';
 
 export default combineReducers({
+    alert
 });
