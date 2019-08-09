1981fb97a06e0bd8f5673d09bdd94122153a5b6c
Peter Gorgone <peter@peltronic.com>
2019-08-08 20:42:42 -0700

    61: Post Item Component

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { connect }  from 'react-redux';
 import { getPosts }  from '../../actions/post';
 import Spinner from '../layout/Spinner';
+import PostItem from './PostItem';
 
 const Posts = ({ 
     getPosts,
@@ -11,13 +12,24 @@ const Posts = ({
         loading
     }
 }) => {
-
     useEffect(() => {
         getPosts();
     }, [getPosts]);
 
-    return <div className="profile bg-light"> 
-        </div>;
+    return loading ? <Spinner /> : (
+        <Fragment>
+            <h1 className="large text-primary">Posts</h1>
+            <p className="lead">
+                <i className="fas fas-user"></i> Welcome to the community
+            </p>
+            {/* PostForm */}
+            <div className="posts">
+                {posts.map(post =>(
+                    <PostItem key={post._id} post={post} />
+                ))}
+            </div>
+        </Fragment>
+    );
 }
 
 Posts.propTypes = {
--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -17,7 +17,7 @@ export default function(state=initialState, action) {
         case GET_POSTS:
             return {
                 ...state,
-                post: payload,
+                posts: payload,
                 loading: false
             };
         case POST_ERROR:
